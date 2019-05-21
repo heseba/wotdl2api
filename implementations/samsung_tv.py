4eb522ace552a12b1e3bcaa537e39279a2dce419
Sebastian Heil <sebastian.kkg@gmx.de>
2019-05-21 15:21:50 +0200

    updated test implementation

--- implementations/samsung_tv.py
+++ implementations/samsung_tv.py
@@ -2,5 +2,8 @@ from flask import Response
 
 print('samsungTV imported')
 
-def switch_on_tv(path_param):
-    return Response(path_param, status=200)
+def switch_on_tv(path_param, power):
+    return Response(path_param + str(power), status=200)
+
+def switch_off_tv():
+    return Response('Running', status=200)
