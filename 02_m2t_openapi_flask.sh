40ea4dfe1d988ff160fee2b68d1016c98f78c7bc
Mahda Noura <mahda.noura@informatik.tu-chemnitz.de>
2020-09-09 12:27:09 +0200

    1. openapi generator version 3.3.4 ruby file for brew installation in dependendies 2. pipfile added with connexion 2.7.0 and swagger-ui 3. added raspberry deployment to step 2 script

--- 02_m2t_openapi_flask.sh
+++ 02_m2t_openapi_flask.sh
@@ -3,13 +3,26 @@ PORT=9000
 MODULE=wot_api
 API=api.yaml
 GENERATOR_PATH=/usr/local/Cellar/openapi-generator/3.3.4/bin
-PYTHON_PATH=~/virtualenvs/WotDL2API_NEW/bin
+PYTHON_PATH=/Users/mahda/.local/share/virtualenvs/wotdl2api-iHq_-KG6/bin
+PI_PYTHON_PATH=/home/pi/.local/share/virtualenvs/wotdl2api-experiment-bAUvETzh/bin
 TARGET=flask_out
+PI_IP=10.0.1.200
 
 ( set -x ; ${GENERATOR_PATH}/openapi-generator generate -i ${API} -g python-flask -o ${TARGET} -c config.json)
 ( set -x ; ${PYTHON_PATH}/python3 post_generation.py ${TARGET} ${MODULE} ${API})
 ( set -x ; cp hub.py ${TARGET}/${MODULE})
 echo
-echo running API, open http://0.0.0.0:${PORT}/api/ui in your browser
+echo copying to raspberry
 echo
+#add for pi
+( set -x ; rsync -av --progress flask_out/* pi@${PI_IP}:/home/pi/wotdl2api-experiment)
+echo
+#echo running API, open http://0.0.0.0:${PORT}/api/ui in your browser
+#add for pi
+echo running API, open http://${PI_IP}:${PORT}/api/ui in your browser
+echo
+( set -x ; ssh -t pi@${PI_IP} "lsof -ti tcp:9000 | xargs kill")
+echo
+#add for pi
+( set -x ;ssh -t pi@${PI_IP} "cd /home/pi/wotdl2api-experiment ; exec ${PI_PYTHON_PATH}/python3 -m ${MODULE}")
 #( set -x ; cd ${TARGET}; ${PYTHON_PATH}/python3 -m ${MODULE})
---
+++ dependencies/readme.txt
@@ -0,0 +1,27 @@
+pip install connexion[swagger-ui]
+
+git clone https://github.com/Homebrew/homebrew-core.git
+cd homebrew-core
+git log 0bded00985a486bd61101210d1264117a458a903 -- Formula/openapi-generator.rb
+cd
+brew extract --version=3.3.4 openapi-generator mahda/custom
+brew extract --version=3.3.4 openapi-generator
+brew extract --version=3.3.4 openapi-generator homebrew/core
+brew extract --version=3.3.4 openapi-generator .
+brew extract --version=3.3.4 openapi-generator test
+brew extract --version=3.3.4 openapi-generator test/test
+cd homebrew-core
+cd Downloads/homebrew-core
+git checkout 434d28a244429616a2bf47a436af8d6655e49010
+l
+vim Formula/openapi-generator.rb
+cp Formula/openapi-generator.rb ../..
+..
+l
+mv openapi-generator.rb Downloads
+cd Downloads
+l
+ruby
+ruby openapi-generator.rb
+vim openapi-generator.rb
+brew install --build-from-source openapi-generator.rb
