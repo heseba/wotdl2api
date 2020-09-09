9c94d9c4bedc859b5b53691fb93c9d70056ee9a5
Mahda Noura <mahda.noura@informatik.tu-chemnitz.de>
2020-09-09 12:46:37 +0200

    variable deployment on raspberry or local added in step 2

--- 02_m2t_openapi_flask.sh
+++ 02_m2t_openapi_flask.sh
@@ -7,22 +7,24 @@ PYTHON_PATH=/Users/mahda/.local/share/virtualenvs/wotdl2api-iHq_-KG6/bin
 PI_PYTHON_PATH=/home/pi/.local/share/virtualenvs/wotdl2api-experiment-bAUvETzh/bin
 TARGET=flask_out
 PI_IP=10.0.1.200
+DEPLOY_ON_PI=false
 
 ( set -x ; ${GENERATOR_PATH}/openapi-generator generate -i ${API} -g python-flask -o ${TARGET} -c config.json)
 ( set -x ; ${PYTHON_PATH}/python3 post_generation.py ${TARGET} ${MODULE} ${API})
 ( set -x ; cp hub.py ${TARGET}/${MODULE})
 echo
-echo copying to raspberry
-echo
-#add for pi
-( set -x ; rsync -av --progress flask_out/* pi@${PI_IP}:/home/pi/wotdl2api-experiment)
-echo
-#echo running API, open http://0.0.0.0:${PORT}/api/ui in your browser
-#add for pi
-echo running API, open http://${PI_IP}:${PORT}/api/ui in your browser
-echo
-( set -x ; ssh -t pi@${PI_IP} "lsof -ti tcp:9000 | xargs kill")
-echo
-#add for pi
-( set -x ;ssh -t pi@${PI_IP} "cd /home/pi/wotdl2api-experiment ; exec ${PI_PYTHON_PATH}/python3 -m ${MODULE}")
-#( set -x ; cd ${TARGET}; ${PYTHON_PATH}/python3 -m ${MODULE})
+if $DEPLOY_ON_PI ; then
+    echo copying to raspberry
+    echo
+    ( set -x ; rsync -av --progress flask_out/* pi@${PI_IP}:/home/pi/wotdl2api-experiment)
+    echo
+    ( set -x ; ssh -t pi@${PI_IP} "lsof -ti tcp:9000 | xargs kill")
+    echo
+    echo running API on raspberry pi, open http://${PI_IP}:${PORT}/api/ui in your browser
+    echo
+    ( set -x ;ssh -t pi@${PI_IP} "cd /home/pi/wotdl2api-experiment ; exec ${PI_PYTHON_PATH}/python3 -m ${MODULE}")
+else
+    echo running API locally, open http://0.0.0.0:${PORT}/api/ui in your browser
+    ( set -x ; cd ${TARGET}; ${PYTHON_PATH}/python3 -m ${MODULE})
+fi
+
